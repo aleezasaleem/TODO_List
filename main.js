6a7e895da2e694e7b0319299236b464d255e5873
Aleeza Saleem <150428514+aleezasaleem@users.noreply.github.com>
2024-01-05 15:25:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+import inquirer from "inquirer";
+async function todolist() {
+    let { task } = await inquirer.prompt([{
+            type: "input",
+            name: "task",
+            message: "What would you like to add?"
+        }]);
+    console.log(`Added task: ${task}`);
+}
+todolist();
