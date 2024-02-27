a594802e41180bb5f604db0b1b878e423d3f054b
Aleeza Saleem <150428514+aleezasaleem@users.noreply.github.com>
2024-02-27 12:34:22 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,35 @@
 import inquirer from "inquirer";
-async function todolist() {
-    let { task } = await inquirer.prompt([{
+let todos = [];
+let loop = true;
+while (loop) {
+    let answer = await inquirer.prompt([
+        {
             type: "input",
-            name: "task",
-            message: "What would you like to add?"
-        }]);
-    console.log(`Added task: ${task}`);
+            name: "TODO",
+            message: "What do you want to add in your todo?"
+        }, {
+            type: "confirm",
+            name: "addmore",
+            message: "Do you want to add more todo?",
+            default: false
+        }
+    ]);
+    let { TODO, addmore } = answer;
+    loop = addmore;
+    if (TODO) {
+        todos.push(TODO);
+    }
+    else {
+        console.log("Kindly add valid input");
+    }
 }
-todolist();
+if (todos.length > 0) {
+    console.log("Your Todo List is:");
+    todos.forEach(todo => {
+        console.log(todo);
+    });
+}
+else {
+    console.log("No todos found");
+}
+
